fbaa2260fb34db5103bf465206a4dc68a514cfd6
Mr_Veit <Misster.veit@gmail.com>
2025-02-08 17:36:14 +1200

    Minor fix

--- src/routes/walletAsseetsRoutes.js
+++ src/routes/walletAsseetsRoutes.js
@@ -1,10 +1,9 @@
-const { createJettonTransactionPayload, 
-    createNftTransactionPayload }  = require('../controllers/walletAssetsController');
+const walletAssets = require('../controllers/walletAssetsController');
 
 const express = require('express');
 const router = express.Router();
 
-router.post('/assets/jetton/payload', createJettonTransactionPayload);
-router.post('/assets/nft/payload', createNftTransactionPayload);
+router.post('/assets/jetton/payload', walletAssets.createJettonTransactionPayload);
+router.post('/assets/nft/payload', walletAssets.createNftTransactionPayload);
 
 module.exports = router;
